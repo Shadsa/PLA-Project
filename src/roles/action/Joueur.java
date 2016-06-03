8ec5f1448080c81edd523f7c3d70097e7f41838e
deliseport <deliseport@deliseport-GP60-2QE>
2016-06-03 16:03:09 +0200

    La base de Joueur

--- src/roles/action/Joueur.java
+++ src/roles/action/Joueur.java
@@ -2,13 +2,23 @@ package roles.action;
 
 import java.util.ArrayList;
 
+import roles.Automate;
 import roles.Personnage;
 
 public class Joueur {
 
+	String _nom;
+	ArrayList<Personnage> _personnages;
+	ArrayList<Automate> _automates;
+
+	public Joueur(String nom, ArrayList<Automate> automates)
+	{
+		_nom = nom;
+		_automates = automates;
+	}
+
 	public ArrayList<Personnage> getPersonnages() {
-		// TODO Auto-generated method stub
-		return null;
+		return _personnages;
 	}
 
 }
