5490239fe6c69a3000e46927b8f091bb8ad48181
Shadsa <zehava-bol@live.fr>
2016-06-07 09:45:49 +0200

    WIP Workshop

--- src/workshop/WorkshopCreator.java
+++ src/workshop/WorkshopCreator.java
@@ -7,17 +7,41 @@ import roles.conditions.Condition;
 
 public class WorkshopCreator {
 	
+	private String filepath = "../../Workshop/";
 	private ArrayList<String> deckActionName;
 	private ArrayList<String> deckConditionName;	
 	private ArrayList<ArrayList<Action>> deckAction;
 	private ArrayList<ArrayList<Condition>> deckCondition;
 	
 	public WorkshopCreator(){
-		
+		deckAction = new ArrayList<ArrayList<Action>>();
+		deckCondition = new ArrayList<ArrayList<Condition>>();
+		deckActionName = new ArrayList<String>();
+		deckConditionName = new ArrayList<String>();
+		load();
 	}
 	
 	public void load(){
 		
+	}
+	
+	
+	public ArrayList<Action> getDeckAction(String name){
+		for(int i=0;i<deckActionName.size();i++){
+			if(deckActionName.get(i)==name){
+				return deckAction.get(deckActionName.indexOf(deckActionName.get(i)));
+			}
+		}
+		return null;
+	}
+	
+	public ArrayList<Condition> getDeckCondition(String name){
+		for(int i=0;i<deckConditionName.size();i++){
+			if(deckConditionName.get(i)==name){
+				return deckCondition.get(deckConditionName.indexOf(deckConditionName.get(i)));
+			}
+		}
+		return null;
 	}
 	
 	public ArrayList<ArrayList<Action>> actionList(){
