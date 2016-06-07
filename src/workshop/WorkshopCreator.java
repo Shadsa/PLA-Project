2d691ce5f78fd62889207fc8b6c21bae84ee0b17
Stelle <estelle.allard2@hotmail.fr>
2016-06-07 09:46:16 +0200

    Workshop load

--- src/workshop/WorkshopCreator.java
+++ src/workshop/WorkshopCreator.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import roles.*;
 import roles.action.Action;
 import roles.conditions.Condition;
+import java.io.File;
 
 public class WorkshopCreator {
 	
@@ -17,6 +18,19 @@ public class WorkshopCreator {
 	}
 	
 	public void load(){
+		File f = new File(filepath+"/action.deck/");
+		//initialiser le tableau
+		ArrayList<File> files=new ArrayList<File>();
+		for (File file : f.listFiles()){
+			files.add(file);
+			// création d'un tableau regroupant tous les fichiers
+		}
+		int x = 0;
+		for (int i = 0 ; i < files.length ; i++) {
+		  if (f[i].isFile()) {
+		    x++;
+		  }
+		}
 		
 	}
 	
