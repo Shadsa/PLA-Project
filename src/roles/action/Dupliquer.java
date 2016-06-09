2766968e044974c26a6552a1c6a54a1a76d152b1
Maxime Dereymez <dereymez.maxime@gmail.com>
2016-06-09 09:59:11 +0200

    dupliquer + createPersonnage renvoie maintenant le personnage créé

---
+++ src/roles/action/Dupliquer.java
@@ -0,0 +1,35 @@
+package roles.action;
+
+import roles.Cardinaux;
+import roles.Personnage;
+import roles.States;
+import roles.States.Statut;
+
+public final class Dupliquer extends Action {
+
+	Cardinaux _direction;
+	private static int _Id = Action.getId(4);
+
+	public Dupliquer(Cardinaux card) {
+		super();
+		_direction = card;
+	}
+
+	@Override
+	public void Act(Personnage pers) {
+		int destX = pers.X() + ((_direction == Cardinaux.OUEST)? (-1) : ((_direction == Cardinaux.EST)? 1 : 0));
+		int destY = pers.Y() + ((_direction == Cardinaux.SUD)? (-1) : ((_direction == Cardinaux.NORD)? 1 : 0));
+		if(World.isfree(destX, destY))
+		{
+			Personnage newPers = pers.owner().createPersonnage(0, destX, destY);
+			World.Case(destX, destY).setPersonnage(newPers);
+			pers.setState(new States(Statut.ATTAQUE, _direction));
+		}
+	}
+
+	@Override
+	public int toInt() {
+		return _Id;
+	}
+
+}
