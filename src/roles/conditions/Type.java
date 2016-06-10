0aea1ea872589abb28524715ebe444c387acc1f4
Maxime Dereymez <dereymez.maxime@gmail.com>
2016-06-10 15:27:49 +0200

    Direction pour type

--- src/roles/conditions/Type.java
+++ src/roles/conditions/Type.java
@@ -8,13 +8,17 @@ import roles.action.World;
 public class Type extends Condition {
 
 	int _type;
+	Cardinaux _direction;
 
-	public Type(TypeCase type) {
+	public Type(TypeCase type, Cardinaux card) {
 		_type = type.value();
+		_direction = card;
 	}
 
 	@Override
 	public boolean value(Personnage target) {
-		return World.Case(target.X(), target.Y()).type().value()==_type;
+		int destX = target.X() + ((_direction == Cardinaux.OUEST)? (-1) : ((_direction == Cardinaux.EST)? 1 : 0));
+		int destY = target.Y() + ((_direction == Cardinaux.NORD)? (-1) : ((_direction == Cardinaux.SUD)? 1 : 0));
+		return World.Case(destX, destY).type().value()==_type;
 	}
 }
