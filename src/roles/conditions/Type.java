efd26bd6534678a16394c328433ba79916b4e395
Maxime Dereymez <dereymez.maxime@gmail.com>
2016-06-10 16:06:25 +0200

    Correction de vérification si case existe

--- src/roles/conditions/Type.java
+++ src/roles/conditions/Type.java
@@ -19,6 +19,6 @@ public class Type extends Condition {
 	public boolean value(Personnage target) {
 		int destX = target.X() + ((_direction == Cardinaux.OUEST)? (-1) : ((_direction == Cardinaux.EST)? 1 : 0));
 		int destY = target.Y() + ((_direction == Cardinaux.NORD)? (-1) : ((_direction == Cardinaux.SUD)? 1 : 0));
-		return World.Case(destX, destY).type().value()==_type;
+		return World.Case(destX, destY)!=null && World.Case(destX, destY).type().value()==_type;
 	}
 }
