7a8d7488996355bc90ad76c9f614f8429572fb7c
lawchris <law.christopher38@gmail.com>
2016-06-12 12:46:44 +0200

    Correction Police importée.

--- src/graphique/Button.java
+++ src/graphique/Button.java
@@ -6,7 +6,11 @@ import java.io.File;
 import java.io.IOException;
 
 import org.newdawn.slick.Color;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
 import org.newdawn.slick.TrueTypeFont;
+import org.newdawn.slick.UnicodeFont;
+import org.newdawn.slick.font.effects.ColorEffect;
 
 public class Button {
 
@@ -14,27 +18,33 @@ public class Button {
 	static Font Rotunda;
 
 	private Font font;
-	private TrueTypeFont ttf;
+	private UnicodeFont ttf;
 	private String _text;
 	public int x;
 	public int y;
 	private int width;
 	private int height;
 
-	public Button(String text, int x, int y)
+	public Button(String text, int x, int y) throws SlickException
 	{
-		font = Rotunda.deriveFont(Font.TRUETYPE_FONT, 12);
+		//font = Rotunda.deriveFont(Font.TRUETYPE_FONT, 12);
 		//font = new Font("src/asset/fonts/Friedolin.ttf", Font.PLAIN, 12);
-	    ttf = new TrueTypeFont(font, true);
+	    ttf = new UnicodeFont("src/asset/fonts/Berry Rotunda.ttf", 12, false, false);
+	    ttf.addAsciiGlyphs();
+	    ttf.getEffects().add(new ColorEffect());
+	    ttf.loadGlyphs();
+
+	    
 	    _text = text;
 	    this.x = x;
 	    this.y = y;
 	    width = ttf.getWidth(_text) + 20;
-	    height = ttf.getHeight() + 16;
+	    height = ttf.getHeight("A") + 16;
 	}
 
-	public void render()
+	public void render(Graphics g)
 	{
+		g.setFont(ttf);
 		InitGameState.UI.draw(x, y, x+23, y+height, 634, 118, 657, 141);
 		InitGameState.UI.draw(x+23, y, x+width-23, y+height, 659, 118, 681, 141);
 		InitGameState.UI.draw(x+width-23, y, x+width, y+height, 683, 118, 707, 141);
