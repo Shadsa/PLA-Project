d246f55be35e1e7ec3c379c24710268685814c21
Shadsa <zehava-bol@live.fr>
2016-06-16 15:46:49 +0200

    Fix Libre, essai

--- src/roles/conditions/Libre.java
+++ src/roles/conditions/Libre.java
@@ -20,8 +20,12 @@ public class Libre extends Condition {
 	public boolean value(Personnage target) {
 		int destX = target.X() + ((_direction == Cardinaux.OUEST)? (-1) : ((_direction == Cardinaux.EST)? 1 : 0));
 		int destY = target.Y() + ((_direction == Cardinaux.NORD)? (-1) : ((_direction == Cardinaux.SUD)? 1 : 0));
-		if(!target.classe().hard_walker() && World.Case(destX, destY) != null  && World.Case(destX, destY).type() instanceof Arbre){
-			return false;
+		if(World.Case(destX, destY) != null  && World.Case(destX, destY).type() instanceof Arbre){
+			if(target.classe().hard_walker()){
+				return p.check(World.Case(destX, destY));
+			}else{
+				return false;
+			}
 		}
 		return p.check(World.Case(destX, destY));
 	}
