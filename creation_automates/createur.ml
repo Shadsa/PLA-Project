f2140d8beff129dc701855154f0163ee478e29cc
Hugo <amodrufavin.hugo@gmail.com>
2016-06-19 19:56:39 +0200

    correctif .ml v3

--- creation_automates/createur.ml
+++ creation_automates/createur.ml
@@ -9,7 +9,7 @@ let ouvrier1 = creerAutomate [errant 1 0 0; chercheur 3 arbreProche [1;2;3;4] 0 
 
 let soldat1 = creerAutomate [errant 1 0 0; chercheur 3 ennemiProche [1;2;3;4] 0 0; appliquerSurListe (hostile 6) [0;1;2;3;4] 0; appliquerSurListe (suiveur 100) [0;1;2;3;4] 0]
 
-let batisseur1 = creerAutomate [errant 1 0 1; errant 1 1 0; reparateur 1 1 0; reparateur 1 0 1; constructeur 1 mur 120 1 0 ; [(0,OrdreDonne,ConstruireMur,1,100)] ; [(1,OrdreDonne,ConstruireMur,0,100)]]
+let batisseur1 = creerAutomate [errant 1 0 1; errant 1 1 0; reparateur 1 1 0; reparateur 1 0 1; constructeur 1 mur 120 1 0 ; [(0,OrdreDonne,ConstruireMur(N),1,100)] ; [(0,OrdreDonne,ConstruireMur(E),1,100)] ; [(0,OrdreDonne,ConstruireMur(S),1,100)] ; [(0,OrdreDonne,ConstruireMur(O),1,100)] ; [(1,OrdreDonne,ConstruireMur(N),0,100)] ; [(1,OrdreDonne,ConstruireMur(E),0,100)] ; [(1,OrdreDonne,ConstruireMur(S),0,100)] ; [(1,OrdreDonne,ConstruireMur(O),0,100)]]
 
 let soigneur1 = creerAutomate [errant 1 0 1; errant 1 1 0; medecin 10 0 1; medecin 10 1 0; constructeur 1 piege 118 1 0]
 
@@ -17,7 +17,7 @@ let ouvrier2 = creerAutomate [errant 1 0 0; chercheur 3 arbreProche [1;2;3;4] 0 
 
 let soldat2 = creerAutomate [errant 1 0 0; chercheur 6 ennemiProche [1;2;3;4] 0 0; appliquerSurListe (hostile 6) [0;1;2;3;4] 0; appliquerSurListe (suiveur 100) [0;1;2;3;4] 0]
 
-let batisseur2 = creerAutomate [errant 1 0 1; errant 1 1 0; reparateur 1 1 0; reparateur 1 0 1; constructeur 1 mur 128 1 0 ; [(0,OrdreDonne,ConstruireMur,1,100)] ; [(1,OrdreDonne,ConstruireMur,0,100)]]
+let batisseur2 = creerAutomate [errant 1 0 1; errant 1 1 0; reparateur 1 1 0; reparateur 1 0 1; constructeur 1 mur 128 1 0 ; [(0,OrdreDonne,ConstruireMur(N),1,100)] ; [(0,OrdreDonne,ConstruireMur(E),1,100)] ; [(0,OrdreDonne,ConstruireMur(S),1,100)] ; [(0,OrdreDonne,ConstruireMur(O),1,100)] ; [(1,OrdreDonne,ConstruireMur(N),0,100)] ; [(1,OrdreDonne,ConstruireMur(E),0,100)] ; [(1,OrdreDonne,ConstruireMur(S),0,100)] ; [(1,OrdreDonne,ConstruireMur(O),0,100)]]
 
 let soigneur2 = creerAutomate [errant 1 0 1; errant 1 1 0; medecin 10 0 1; medecin 10 1 0; constructeur 1 piege 124 1 0; constructeur 1 piege 124 0 1]
 
