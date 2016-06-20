d38a4fadbb65053d591ca948afd825c5a2c77854
Hugo <amodrufavin.hugo@gmail.com>
2016-06-20 10:52:43 +0200

    LAST

--- creation_automates/createur.ml
+++ creation_automates/createur.ml
@@ -13,7 +13,7 @@ let batisseur1 = creerAutomate [errant 1 0 1; errant 1 1 0; reparateur 1 1 0; re
 
 let soigneur1 = creerAutomate [errant 1 0 1; errant 1 1 0; medecin 10 0 1; constructeur 1 piege 103 1 0; [(0,Et(Libre(N),OrdreDonne),piege N,0,100)]; [(0,Et(Libre(S),OrdreDonne),piege S,0,100)]; [(0,Et(Libre(O),OrdreDonne),piege O,0,100)]; [(1,Et(Libre(N),OrdreDonne),piege N,0,100)]; [(1,Et(Libre(E),OrdreDonne),piege E,0,100)]; [(1,Et(Libre(S),OrdreDonne),piege S,0,100)]; [(1,Et(Libre(O),OrdreDonne),piege O,0,100)]]
 
-let ouvrier2 = creerAutomate [errant 1 0 0; chercheur 3 arbreProche [1;2;3;4] 0 0; createurNbRatio 10 0 100 5 25 0 0; createur 8 1 100 0 0; createur 8 2 110 0 0; createur 8 3 130 0 0; appliquerSurListe (recolteur 5) [0;1;2;3;4] 0 ; appliquerSurListe (suiveur 100) [0;1;2;3;4] 0]
+let ouvrier2 = creerAutomate [errant 1 0 0; chercheur 3 arbreProche [1;2;3;4] 0 0; createurNbRatio 10 0 100 5 25 0 0; createur 8 1 100 0 0; createur 8 2 109 0 0; createur 8 3 109 0 0; appliquerSurListe (recolteur 5) [0;1;2;3;4] 0 ; appliquerSurListe (suiveur 100) [0;1;2;3;4] 0]
 
 let soldat2 = creerAutomate [errant 1 0 0; chercheur 5 ennemiProche [1;2;3;4] 0 0; appliquerSurListe (hostile 6) [0;1;2;3;4] 0; appliquerSurListe (suiveur 4) [0;1;2;3;4] 0]
 
